d23a63a820d2fc0d1f7989d106c365b2bd53a2aa
Tri Bintang Pamungkas <93517500+bintangpmks@users.noreply.github.com>
2022-01-08 00:32:44 +0700

    Add files via upload

---
+++ praktikum 1 latihan 1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 12 19:34:17 2021
+
+@author: Acer
+"""
+
+"""
+@materi : variabel dan pemograman dasar
+@judul : pratikum 1 materi 
+@hari/tanggal : senin, 20210920
+@nim : 065002100023
+@author : Tri Bintang Pamungkas
+"""
+
+
+
+x = input("masukan namamu:  ") 
+print(" hallo " + x)
+
+x = input("dimana kamu tinggal: ")
+print("waah ternyata kamu tinggal di " + x)
+
+x = input("asal sekolah kamu dari mana: ")
+print("ternyata dari " + x)
+
+x = input("kecamatan kamu dari mana: ")
+print("owh dari  " + x)
